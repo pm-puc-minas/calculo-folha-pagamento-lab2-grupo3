a581a7decef6c606f1210244ff7ac583ebd056aa
DiogoAugustoMM <128488227+DiogoAugustoMM@users.noreply.github.com>
2025-08-27 17:58:09 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/gestaopagamento/gestaopagamento/GestaoPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaopagamento.gestaopagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestaopagamento/gestaopagamento/GestaoPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaopagamento.gestaopagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
