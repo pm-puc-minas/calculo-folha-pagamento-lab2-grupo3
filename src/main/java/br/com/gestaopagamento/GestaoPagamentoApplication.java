8c015c8dc5fb0724a8da6d6592aab9dca9ea065e
DiogoAugustoMM <128488227+DiogoAugustoMM@users.noreply.github.com>
2025-09-10 15:25:09 -0300

    Adiciona meus primeiros arquivos do trabalho

--- src/main/java/br/com/gestaopagamento/GestaoPagamentoApplication.java
+++ src/main/java/br/com/gestaopagamento/GestaoPagamentoApplication.java
@@ -1,4 +1,4 @@
-package br.com.gestaopagamento.gestaopagamento;
+package br.com.gestaopagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
