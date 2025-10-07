926b923c240f8c52f0b2c2a685a2732c518b4fe2
João Pedro <jplisboa.dev@gmail.com>
2025-10-07 10:03:01 -0300

    feat: iniciar lógica CalcularIRRF

--- src/main/java/br/com/gestaopagamento/Service/impl/CalcularIRRF.java
+++ src/main/java/br/com/gestaopagamento/Service/impl/CalcularIRRF.java
@@ -6,11 +6,28 @@ import br.com.gestaopagamento.Models.Funcionario;
 import br.com.gestaopagamento.Service.Desconto;
 
 public class CalcularIRRF implements Desconto {
-
+    private static final BigDecimal DeducaoPorDependete = new BigDecimal("189,59");
 
      @Override
     public BigDecimal calcular(Funcionario funcionario){
-        //retornando zero apenas para não dar erro
+        //Calcula o salário base
+        Desconto calculaInss = new CalcularINSS();
+        BigDecimal valorInss = calculaInss.calcular(funcionario);
+
+        BigDecimal salarioBase = funcionario.getSalarioBruto().subtract(valorInss);
+
+        //Calcula a base de cálculo
+        BigDecimal qntdDependentes = BigDecimal.valueOf(funcionario.getQntdDependentes());
+        BigDecimal pensaoAlimenticia = BigDecimal.valueOf(funcionario.getPensaoAlimenticia());
+        BigDecimal outrasDeducoes = BigDecimal.valueOf(funcionario.getOutrasDeducoes());
+
+        BigDecimal baseDeCalculo = salarioBase.subtract(qntdDependentes.multiply(DeducaoPorDependete));
+        salarioBase = salarioBase.subtract(pensaoAlimenticia).subtract(outrasDeducoes);
+
+
+
+
+
         return BigDecimal.ZERO;
     }
 }
