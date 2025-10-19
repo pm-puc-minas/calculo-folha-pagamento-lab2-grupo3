4533b4111214f0f7807aaf69142f830e11789e95
Paulo Coelho <paulohdscoelho@gmail.com>
2025-10-19 15:46:07 -0300

    [NEW] correção da sprint 2

--- src/main/java/br/com/gestaopagamento/Service/Adicional.java
+++ src/main/java/br/com/gestaopagamento/Service/Adicional.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.gestaopagamento.Models.Funcionario;
 
+//Intefaces em Java precisam começar com I: IAdicional
 public interface Adicional {
 
      BigDecimal calcular(Funcionario funcionario);
