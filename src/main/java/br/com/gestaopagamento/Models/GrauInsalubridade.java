0e01ffc5716fce159e1409616ab9d799f12bccd0
antonio <antoniohenrique260704@gmail.com>
2025-10-24 00:28:37 -0300

    java - BD

--- src/main/java/br/com/gestaopagamento/Models/GrauInsalubridade.java
+++ src/main/java/br/com/gestaopagamento/Models/GrauInsalubridade.java
@@ -1,8 +1,32 @@
 package br.com.gestaopagamento.Models;
 
 public enum GrauInsalubridade {
-    NENHUM,
-    BAIXO,
-    MEDIO,
-    ALTO
+    // Adicione o NENHUM(0) para casos onde não se aplica
+    NENHUM(0), 
+    BAIXO(1),
+    MEDIO(2),
+    ALTO(3);
+
+    private final int valor;
+
+    GrauInsalubridade(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    // ESTE É O MÉTODO QUE ESTÁ FALTANDO
+    public static GrauInsalubridade fromInt(int grau) {
+        switch (grau) {
+            case 0: return NENHUM; // Adicionamos o caso 0
+            case 1: return BAIXO;
+            case 2: return MEDIO;
+            case 3: return ALTO;
+            default: 
+                // Lança um erro se o número for inválido
+                throw new IllegalArgumentException("Grau de insalubridade inválido: " + grau);
+        }
+    }
 }
