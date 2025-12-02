152a012acaf46e05147bfccaa6f8c159482fc77f
JP-devone <1205493@sga.pucminas.br>
2025-12-02 02:38:08 -0300

    fix: corrigindo alguns bugs do filtro de segurança, organização das pastas static e melhorando o front

--- src/main/java/br/com/gestaopagamento/infra/security/SecurityFilter.java
+++ src/main/java/br/com/gestaopagamento/infra/security/SecurityFilter.java
@@ -25,13 +25,20 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
+
         if (token != null) {
             var login = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByLogin(login);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            if (login != null) {
+                UserDetails user = userRepository.findByLogin(login);
+
+                if (user != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(user,null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
         }
+
         filterChain.doFilter(request, response);
     }
     private String recoverToken(HttpServletRequest request){
